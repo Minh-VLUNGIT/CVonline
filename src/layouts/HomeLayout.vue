70a21e15cdfc23e76237a1135aee125a7c9215e5
vluit17 <minh2002hoang@gmail.com>
2023-12-23 21:24:22 +0700

    .

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="hello">
+      <div class="md:max-w-6xl mx-auto  top-0 z-50 h-full	">
+        <div class="bg-white flex flex-wrap items-center justify-center px-10 md:py-4 py-2 overflow-hidden border-b border-gray-200 font-2xl sm:px-4 md:overflow-visible md:px-2">
+          <h2 class="text-7xl mt-10" v-on:click="reverseMessage('/CVvn/home')"  :style="{cursor: 'pointer' }"> XEM CV NGUYỄN HOÀNG MINH</h2> 
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+
+  methods: {
+    reverseMessage(link) {
+      this.$router.push(link); // Chuyển hướng đến todo.link
+    }
+  },
+  computed: {
+    olStyles() {
+      return {
+        borderBottom: '1px solid #000',
+        position:'fixed',
+        width:'85%',
+        height: '100px',
+        left: '50%',
+        top:'50px',
+        transform: 'translate(-50%, -50%)',
+        zIndex:'1',
+        background:'white'
+      };
+    },
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 0 0 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
--- src/router/home.js
+++ src/router/home.js
@@ -1,24 +1,7 @@
 const home = [
   {
     path: "/",
-    component: () => import("../layouts/AdminLayout.vue"),
-    children: [
-      {
-        path: "home",
-        name: "admin-users",
-        component: () => import("../pages/admin/usermana/IndexPage.vue"),
-      },
-      {
-        path: "project",
-        name: "admin-project",
-        component: () => import("../pages/admin/userproject/IndexPage.vue"),
-      },
-      {
-        path: "project/:id",
-        name: "admin-viewwproject",
-        component: () => import("../pages/admin/viewproject/IndexPage.vue"),
-      },
-    ],
+    component: () => import("../layouts/HomeLayout.vue"),
   },
 ];
 
